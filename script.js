629cab0250539d76b0eba51556b96259a5de426e
jacoblovelace <jtl5jjn@virginia.edu>
2024-03-27 17:19:01 -0400

    Added background particles and hue shift to background effect

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
-
-// TITLE SCREEN
 var welcome = document.getElementById('welcome-screen');
 var container = document.getElementById('container');
 var content = document.getElementById('content');
 var nav = document.getElementById('nav');
 var footer = document.getElementById('footer');
 
+// Adjust brightness of background image based on position relative to header and footer elements
 function adjustBackgroundBrightness() {
 
   const headerRect = welcome.getBoundingClientRect();
   const footerRect = footer.getBoundingClientRect();
 
-  const lowestBrightness = 10;
-  const highestBrightness = 40;
+  const lowestBrightness = 20;
+  const highestBrightness = 50;
 
   let backgroundBrightness;
 
@@ -30,48 +29,47 @@ function adjustBackgroundBrightness() {
   }
 
   document.getElementById('body-bg').style.filter = `brightness(${backgroundBrightness}%)`;
+  document.getElementById('particles-js').style.background = `rgba(${40 - (1 * backgroundBrightness)}, 0, ${120 - (3 * backgroundBrightness)}, 0.3)`;
 }
 
-// Arrow animation on title screen
+// Start title screen arrow animation and adjust background brightness on DOM content load
 document.addEventListener("DOMContentLoaded", function() {
 
+  // repeating arrow animation
+  function animateArrow() {
+      var downArrow = document.getElementById("down-arrow");
 
-    function animateArrow() {
-        var downArrow = document.getElementById("down-arrow");
+      // delete prev child before creating a new one
+      var animatedArrows = downArrow.querySelectorAll("#animated-arrow");
+      animatedArrows.forEach(function(arrow) {
+      downArrow.removeChild(arrow);
+      });
 
-        // delete prev child before creating a new one
-        var animatedArrows = downArrow.querySelectorAll("#animated-arrow");
-        animatedArrows.forEach(function(arrow) {
-        downArrow.removeChild(arrow);
-        });
+      // clone arrow with new id and append as child to original arrow
+      var animatedArrow = downArrow.cloneNode(true);
+      animatedArrow.setAttribute("id", "animated-arrow");
+      downArrow.appendChild(animatedArrow);
 
-        // clone arrow with new id and append as child to original arrow
-        var animatedArrow = downArrow.cloneNode(true);
-        animatedArrow.setAttribute("id", "animated-arrow");
-        downArrow.appendChild(animatedArrow);
-
-        animatedArrow.animate([
-        { opacity: 1, transform: 'translateY(0)' },
-        { opacity: 0, transform: 'translateY(25px)' }
-        ], {
-        duration: 1500,
-        fill: 'forwards'
-        });
-    }
+      animatedArrow.animate([
+      { opacity: 1, transform: 'translateY(0)' },
+      { opacity: 0, transform: 'translateY(25px)' }
+      ], {
+      duration: 1500,
+      fill: 'forwards'
+      });
+  }
 
   adjustBackgroundBrightness();
-  setInterval(animateArrow, 1500); // new arrow every 1.5 seconds
+  setInterval(animateArrow, 1500);
 });
 
-// darken screen effect
+// Adjust background brightness as needed on scroll events
 document.addEventListener("scroll", function() {
-
   adjustBackgroundBrightness();
 })
 
-
-// TOGGLE CARDS
-// ensure only one box can be open at a time
+// TOGGLE BOX
+// Ensure only one toggle box can be open at a time on click event
 $(".box").click(function() {
   if ($(this).not('open')) {
     $(this).addClass('open');
@@ -80,7 +78,7 @@ $(".box").click(function() {
 });
 
 // MOUSE OVER CARD ROTATION ANIMATION
-// Script outline from: https://codepen.io/armandocanals/pen/WWXZMB
+// Script based off of: https://codepen.io/armandocanals/pen/WWXZMB
 
 let constrain = 50;
 let mouseOverContainers = document.getElementsByClassName("project-card");
@@ -111,8 +109,7 @@ for (let i = 0; i < layers.length; i++) {
   };
 }
 
-
-// RESET CARD ROTATION
+// RESET CARD ROTATION ON MOUSELEAVE
 
 function resetTransforms(x, y, el) {
   return "perspective(100px) "
@@ -134,3 +131,116 @@ for (let i = 0; i < layers.length; i++) {
     });
   };
 }
+
+/* ---- particles.js config ---- */
+
+particlesJS("particles-js", {
+  "particles": {
+    "number": {
+      "value": 50,
+      "density": {
+        "enable": false,
+        "value_area": 800
+      }
+    },
+    "color": {
+      "value": "#ffffff"
+    },
+    "shape": {
+      "type": ["circle"],
+      "stroke": {
+        "width": 0,
+        "color": "#ffffff"
+      },
+      "polygon": {
+        "nb_sides": 7
+      },
+      "image": {
+        "src": "img/github.svg",
+        "width": 100,
+        "height": 100
+      }
+    },
+    "opacity": {
+      "value": 0.7,
+      "random": true,
+      "anim": {
+        "enable": true,
+        "speed": 0.5,
+        "opacity_min": 0,
+        "sync": false
+      }
+    },
+    "size": {
+      "value": 4,
+      "random": true,
+      "anim": {
+        "enable": false,
+        "speed": 4,
+        "size_min": 1,
+        "sync": false
+      }
+    },
+    "line_linked": {
+      "enable": false,
+      "distance": 100,
+      "color": "#ffffff",
+      "opacity": 0.1,
+      "width": 1
+    },
+    "move": {
+      "enable": true,
+      "speed": 3,
+      "direction": "none",
+      "random": true,
+      "straight": false,
+      "out_mode": "out",
+      "bounce": false,
+      "attract": {
+        "enable": true,
+        "rotateX": 600,
+        "rotateY": 1200
+      }
+    }
+  },
+  "interactivity": {
+    "detect_on": "window",
+    "events": {
+      "onhover": {
+        "enable": true,
+        "mode": "bubble"
+      },
+      "onclick": {
+        "enable": false,
+        "mode": "none"
+      },
+      "resize": false
+    },
+    "modes": {
+      "grab": {
+        "distance": 140,
+        "line_linked": {
+          "opacity": 0.5
+        }
+      },
+      "bubble": {
+        "distance": 300,
+        "size": 4,
+        "duration": 3,
+        "opacity": 1,
+        "speed": 1
+      },
+      "repulse": {
+        "distance": 200,
+        "duration": 0.4
+      },
+      "push": {
+        "particles_nb": 2
+      },
+      "remove": {
+        "particles_nb": 2
+      }
+    }
+  },
+  "retina_detect": true
+});
